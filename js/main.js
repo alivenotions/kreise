43cf7733822fad4bd1370ed99c528b057d54a5bb
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-12-24 23:08:37 +0100

    clean slate

--- js/main.js
+++
@@ -1,5 +0,0 @@
-function onScroll() {
-  document.querySelector('.container').style.setProperty('--god-var', `${window.scrollY * 0.1}`)
-}
-
-window.addEventListener('scroll', onScroll, {capture: false, passive: true})
