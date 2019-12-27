1d3aafe103e7cdd6e69533fc0c7d1a948fdaab31
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-12-27 04:43:53 +0000

    hover style on image

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const topLayer = document.getElementsByClassName('top')[0]
 const topLayerContent = document.querySelector('.top div')
 function transitionTopLayer () {
   topLayer.style = 'transform: translateY(-100vh)'
-  topLayerContent.style = 'transform: translateY(75vh)'
+  topLayerContent.style = 'transform: translateY(70vh)'
 }
