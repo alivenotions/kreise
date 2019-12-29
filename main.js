df6eacbfcf37c83c0946065687a32f3f00b972ea
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-12-29 18:35:40 +0000

    change structure and BEM the classnames

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const topLayer = document.getElementsByClassName('top')[0]
-const topLayerContent = document.querySelector('.top div')
+const topLayer = document.getElementsByClassName('content--first')[0]
+const topLayerContent = document.querySelector('.content--first div')
 function transitionTopLayer () {
   topLayer.style = 'transform: translateY(-100vh)'
   topLayerContent.style = 'transform: translateY(70vh)'
